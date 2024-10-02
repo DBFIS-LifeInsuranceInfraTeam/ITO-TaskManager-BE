d89b7ca5989baed6542c1307127add8bf17b2a03
unknown <gb.lee@dbfis.co.kr>
2024-10-02 13:54:57 +0900

    Feat: BaseResponseDTO 구조 고려하여 응답 객체 구조 수정

--- src/main/java/com/ITOPW/itopw/dto/response/LoginResponseDTO.java
+++ src/main/java/com/ITOPW/itopw/dto/response/LoginResponseDTO.java
@@ -1,4 +1,21 @@
 package com.ITOPW.itopw.dto.response;
 
+import com.ITOPW.itopw.entity.User;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginResponseDTO {
+    private String token;      // JWT 토큰
+    private int expiresIn;     // 토큰 유효 기간 (초 단위)
+    private UserInfoResponseDto userInfo;         // 로그인한 사용자 정보
+
+
+    // 파라미터가 있는 생성자
+    public LoginResponseDTO(String token, int expiresIn, UserInfoResponseDto userInfo) {
+        this.token = token;
+        this.expiresIn = expiresIn;
+        this.userInfo = userInfo;
+    }
 }
--- src/main/java/com/ITOPW/itopw/dto/response/SignupResponseDTO.java
+++ src/main/java/com/ITOPW/itopw/dto/response/SignupResponseDTO.java
@@ -9,5 +9,10 @@ public class SignupResponseDTO {
 
     private String id;  // 사용자 ID
     private String name;  // 사용자 이름
-    private String message;  // 응답 메시지 (예: "회원가입 성공")
+
+    // 생성자
+    public SignupResponseDTO(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
