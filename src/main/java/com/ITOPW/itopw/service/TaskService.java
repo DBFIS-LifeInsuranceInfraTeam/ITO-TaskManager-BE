57fab7096b89413d33b706bd805028934abdf93d
YoosukByun <qusdbtjr91@naver.com>
2024-10-21 23:49:13 +0900

    태스크 삭제, 수정, 전체조회 기능 추가

--- src/main/java/com/ITOPW/itopw/service/TaskService.java
+++ src/main/java/com/ITOPW/itopw/service/TaskService.java
@@ -3,9 +3,15 @@ package com.ITOPW.itopw.service;
 import com.ITOPW.itopw.entity.Task;
 import com.ITOPW.itopw.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,5 +29,36 @@ public class TaskService {
 
     public Optional<Task> getTaskById(Integer taskId) {
         return taskRepository.findById(taskId); // ID로 태스크 조회
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll(); // 전체 태스크를 반환
+    }
+
+    public boolean deleteTask(Integer id) {
+        Optional<Task> task = taskRepository.findById(id);
+        if (task.isPresent()) {
+            taskRepository.delete(task.get());
+            return true; // 삭제 성공
+        }
+        return false; // 태스크가 존재하지 않음
+    }
+
+    public Optional<Task> updateTask(Integer id, Task task) {
+        Optional<Task> existingTask = taskRepository.findById(id);
+        if (existingTask.isPresent()) {
+            Task updatedTask = existingTask.get();
+            // 기존 Task의 정보를 업데이트합니다.
+            updatedTask.setTaskName(task.getTaskName());
+            updatedTask.setAssigneeId(task.getAssigneeId());
+            updatedTask.setItoProcessId(task.getItoProcessId());
+            updatedTask.setStartDate(task.getStartDate());
+            updatedTask.setDueDate(task.getDueDate());
+            updatedTask.setDescription(task.getDescription());
+            // 필요에 따라 다른 필드도 업데이트할 수 있습니다.
+
+            return Optional.of(taskRepository.save(updatedTask));
+        }
+        return Optional.empty(); // 태스크가 존재하지 않음
     }
 }
