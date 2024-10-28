1ca9864f0b91fc755566af8b9ad598b237df93a6
gyubinlee-dbfis <gb.lee@dbfis.co.kr>
2024-10-28 11:23:55 +0900

    Feat: WebConfig, SecurityConfig 수정

--- src/main/java/com/ITOPW/itopw/config/SecurityConfig.java
+++ src/main/java/com/ITOPW/itopw/config/SecurityConfig.java
@@ -5,26 +5,51 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable) // CSRF 보호 비활성화
-                .authorizeHttpRequests(authorize -> authorize  // authorizeRequests() 대신 authorizeHttpRequests() 사용
-                        .requestMatchers("/api/**").permitAll()  // API 경로에 대한 인증 허용
-                        .requestMatchers("/images/**").permitAll()  // 이미지 경로에 대한 인증 허용
-                        .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/**", "/images/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .cors(withDefaults())  // CORS 설정 활성화
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // 세션을 Stateless로 설정
                 );
 
         return http.build();
     }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(List.of("Content-Type", "Authorization", "Accept"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
