eebbe223e90ad08ffa6d147cc8df4538f6b18ce2
gyubinlee-dbfis <gb.lee@dbfis.co.kr>
2024-10-28 11:06:48 +0900

    Feat: WebConfig ì¶및 SecurityConfig 수정

--- src/main/java/com/ITOPW/itopw/config/WebConfig.java
+++ src/main/java/com/ITOPW/itopw/config/WebConfig.java
@@ -8,8 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/api/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET","POST","DELETE");
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Content-Type", "Authorization", "Accept");
     }
 }
