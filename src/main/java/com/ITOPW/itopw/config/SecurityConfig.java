cdaab207d57218c16429989b991ba495e699e223
gyubinlee-dbfis <gb.lee@dbfis.co.kr>
2024-10-28 10:56:04 +0900

    Feat: WebConfig 추가

--- src/main/java/com/ITOPW/itopw/config/SecurityConfig.java
+++ src/main/java/com/ITOPW/itopw/config/SecurityConfig.java
@@ -13,18 +13,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable) // CSRF 보호 비활성화
-                .authorizeHttpRequests(authorize -> authorize  // authorizeRequests() 대신 authorizeHttpRequests() 사용
-                        .requestMatchers("/api/**").permitAll()  // API 경로에 대한 인증 허용
-                        .requestMatchers("/images/**").permitAll()  // 이미지 경로에 대한 인증 허용
-                        .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
-                );
-
-        return http.build();
-    }
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(AbstractHttpConfigurer::disable) // CSRF 보호 비활성화
+//                .authorizeHttpRequests(authorize -> authorize  // authorizeRequests() 대신 authorizeHttpRequests() 사용
+//                        .requestMatchers("/api/**").permitAll()  // API 경로에 대한 인증 허용
+//                        .requestMatchers("/images/**").permitAll()  // 이미지 경로에 대한 인증 허용
+//                        .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
+//                );
+//
+//        return http.build();
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
